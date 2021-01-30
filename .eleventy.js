98730cd128fdca97b20bce43e49a42c9e140063f
dandevri <hi@dandevri.es>
2021-01-30 21:14:46 +0100

    Add chart.js template

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("stats.js");
   
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
